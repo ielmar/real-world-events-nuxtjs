845d564219b895a3380f04443595f58909f7a0e1
Elmar Ismayilov <bamazy@gmail.com>
2023-09-23 07:50:49 +0800

    create route added

---
+++ components/NavBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="ml-10 flex items-baseline space-x-4">
+    <a
+      v-for="item in navigation"
+      :key="item.name"
+      :href="item.href"
+      :class="[
+        item.current
+          ? 'bg-gray-900 text-white'
+          : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+        'rounded-md px-3 py-2 text-sm font-medium',
+      ]"
+      :aria-current="item.current ? 'page' : undefined"
+      >{{ item.name }}</a
+    >
+  </div>
+</template>
+
+<script setup>
+const navigation = [
+  { name: 'Events', href: '/', current: true },
+  { name: 'Create Event', href: '/create', current: false },
+]
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <header class="bg-gray-800">
+      <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div class="flex h-16 items-center justify-between">
+          <div class="flex items-center">
+            <div class="flex-shrink-0">
+              <nuxt-link to="/">
+                <img
+                  class="h-8 w-8"
+                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                  alt="Your Company"
+                />
+              </nuxt-link>
+            </div>
+            <div class="hidden md:block">
+              <NavBar />
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+    <header class="bg-white shadow">
+      <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        <h1 class="text-3xl font-bold tracking-tight text-gray-900">Events</h1>
+      </div>
+    </header>
+    <main>
+      <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8"><nuxt /></div>
+    </main>
+  </div>
+</template>
+<script>
+import NavBar from '~/components/NavBar.vue'
+export default {
+  components: {
+    NavBar,
+  },
+}
+</script>
